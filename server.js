28985ca78e64c3eebb64b1204031dedc1cc17b61
live4code <live4code@gmail.com>
2015-03-30 07:48:51 +0000

    commit exercise content by Live4Code

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 
 app.use('/', express.static(path.join(__dirname, '')));
