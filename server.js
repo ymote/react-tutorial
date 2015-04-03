1aad6310e53c72c8de4ab409a423581bb531cc7e
live4code <live4code@gmail.com>
2015-04-03 06:37:42 +0000

    commit exercise content by Live4Code

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-/**
- * This file provided by Facebook is for non-commercial testing and evaluation purposes only.
- * Facebook reserves all rights not expressly granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
@@ -22,14 +10,14 @@ app.use(methodOverride());
 
 app.use('/', express.static(path.join(__dirname, '')));
 
-app.get('/comments.json', function(req, res) {
+app.get('/_comments.json', function(req, res) {
   fs.readFile('_comments.json', function(err, data) {
     res.setHeader('Content-Type', 'application/json');
     res.send(data);
   });
 });
 
-app.post('/comments.json', function(req, res) {
+app.post('/_comments.json', function(req, res) {
   fs.readFile('_comments.json', function(err, data) {
     var post = JSON.parse(data);
     post.comments.push(req.body);
