c6649a4ff12085c24005a72f8a144b4b6d68eaba
live4code <live4code@gmail.com>
2015-04-19 22:23:58 +0000

    commit exercise content by Live4Code

--- app/app-cm-movie.js
+++ app/app-cm-movie.js
@@ -1,63 +1,43 @@
 /* CommentBox Component*/
- 
-   
-     
-       
-          
-       
-     
-   
- 
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="detailBox">
+        <div className="titleBox">
+          <label>Comment Box</label>
+          <button type="button" className="close" aria-hidden="true">&times;</button>
+        </div>
+        <div className="commentBox">
+          <p className="taskDescription">Hello, world! I am a post.</p>
+        </div>
+         
+           
+           
+         
+      </div>
+    );
+  }
+});
 
- 
-   
-   
- 
+React.render(
+  <CommentBox />,
+  document.getElementById('content')
+); 
 /* end CommentBox */
 @@@
-prompt: Let's code CommentBox
-run: {command: "undo"}
-run: {command: "undo"}
-run: {command: "undo"}
-run: {command: "undo"}
-run: {command: "undo"}
-run: {command: "undo"}
-run: {command: "undo"}
-run: {command: "undo"}
-run: {command: "undo"}
-run: {command: "undo"}
-run: {command: "undo"}
-run: {command: "undo"}
-run: {command: "undo"}
-moveTo: 1:0
-type: var CommentBox = React.createClass({
-tooltip: {text: "Create a component class -- CommentBox"}
-moveTo:2:2
-type: render: function() {
-tooltip: {text: "Render method is required, it return a single child element -- a virtual representation of a native DOM component"}
-moveTo: 3:4
-type: return (
-moveTo: 4:6
-type: <div className="commentBox">
-tooltip: {text: "This is plain HTML. Notice 'className' is the name of CSS class applied to the div"}
-moveTo: 5:8
-type: Hello, world! I am a CommentBox.
-moveTo: 6:6
+prompt: Add List and Form
+type: "undo"
+type: "undo"
+type: "undo"
+type: "undo"
+moveTo: 12:6
+type: <div className="actionBox">
+moveTo: 13:8
+type: <CommentList />
+tooltip: "Add the CommentList React Component"
+moveTo: 14:8
+type: <CommentForm />
+tooltip: "Add the CommentForm React Component"
+moveTo: 15:6
 type: </div>
-moveTo: 7:4
-type: );
-moveTo: 8:2
-type: }
-tooltip: {text: "Created the DOM element to show on page for the CommentBox"}
-moveTo: 9:0
-type: });
-tooltip: {text: "Right now CommentBox is a static component, so only render method is required"}
-moveTo: 11:0
-type: React.render(
-moveTo: 12:2
-type: <CommentBox />,
-moveTo: 13:2
-type: document.getElementById('content')
-moveTo: 14:0
-type: );
-tooltip: {text: "React.render renders a ReactElement into the DOM in the supplied container. The ReactElement is the CommentBox created before, and the container is the div#content in index.html", wait:8000}
+tooltip: "Now take a look at the CommentList"
