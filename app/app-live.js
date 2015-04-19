fd2ab43f719a581ee35f5dfecce85c284dcc7283
live4code <live4code@gmail.com>
2015-04-19 23:35:59 +0000

    commit exercise content by Live4Code

---
+++ app/app-live.js
@@ -0,0 +1,26 @@
+/* CommentBox Component*/
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="detailBox">
+        <div className="titleBox">
+          <label>Comment Box</label>
+          <button type="button" className="close" aria-hidden="true">&times;</button>
+        </div>
+        <div className="commentBox">
+          <p className="taskDescription">Hello, world! I am a post.</p>
+        </div>
+      <div className="actionBox">   
+        <CommentList />   
+        <CommentForm />   
+      <   
+      </div>
+    );
+  }
+});
+
+React.render(
+  <CommentBox />,
+  document.getElementById('content')
+); 
+/* end CommentBox */
