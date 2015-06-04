a2f3308faef59932d2ffbc46bcea6238f049f55f
Yue Chen <dspfac@gmail.com>
2015-06-04 07:04:49 +0000

    commit exercise content by Live4Code

--- app/app.js
+++ app/app.js
@@ -1,8 +1,14 @@
 var CommentBox = React.createClass({
   render: function() {
     return (
+      <div className="detailBox">
+       <div className="titleBox">
+        <label>Comment Box</label>
+         <button type="button" className="close" aria-hidden="true">&times;</button>
+      </div>
       <div className="commentBox">
         Hello, world! I am a CommentBox. 
+      </div>
       </div>
     );
   }
