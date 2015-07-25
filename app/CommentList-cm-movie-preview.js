39a1b42d3e14e1375103110f7a36c5a7cc07afa6
Yue Chen <dspfac@gmail.com>
2015-07-25 00:50:12 +0000

    commit exercise content by Live4Code

---
+++ app/CommentList-cm-movie-preview.js
@@ -0,0 +1,9 @@
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <ul className="commentList">
+         <Comment author="me">React is all about Components</Comment>
+      </ul>
+    );
+  }
+});
